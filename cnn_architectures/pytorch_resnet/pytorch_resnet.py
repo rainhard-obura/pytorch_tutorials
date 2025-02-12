abf69ceb06e3be0d17ee205ce3bd37606bae954a
rainhard-obura <obura.rainhard@gmail.com>
2025-02-12 19:47:41 +0300

    updated file

--- cnn_architectures/pytorch_resnet/pytorch_resnet.py
+++ cnn_architectures/pytorch_resnet/pytorch_resnet.py
@@ -10,4 +10,14 @@ import torch
 import torch.nn as nn
 
 class block(nn.Module):
-    def __init__(self, channels, intermediate_channels, identity_dowm):
+    def __init__(self, in_channels, intermediate_channels, identity_downsample=None, stride=1):
+        super().__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            intermediate_channels,
+            kernel_size = 1,
+            stride = 1,
+            padding = 0,
+            bias = False
+        )
